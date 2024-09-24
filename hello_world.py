9eac0a9f8d335abfc5c97dcf21d89b2d27719f51
RowSR <rowanstupplebeenrussell@gmail.com>
2024-09-24 21:48:40 +0000

    Update hello_world.py by asking usesr for name

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,12 @@
-
+"""
+This is my first program!
+Rowan Stupplebeen-Russell - 2024
+"""
 
 
 def main():
-  pass
-
+  name: str = input("What is your name? ")
+  print("Hello", name, "!")
 
 
 if __name__ == "__main__":
